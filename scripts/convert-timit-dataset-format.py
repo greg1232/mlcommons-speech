16eb00fcd702c131070d9f2f445b7d0468ad0c59
greg1232 <greg@Landing-JKJG5J.local>
2019-11-26 16:46:18 -0800

    ENH: Added common voice convert script.

--- scripts/convert-timit-dataset-format.py
+++ scripts/convert-timit-dataset-format.py
@@ -9,11 +9,11 @@ import shutil
 logger = logging.getLogger(__name__)
 
 def main():
-    parser = ArgumentParser("This program converts the TIMIT data format to "
+    parser = ArgumentParser("This program converts the COMMON-VOICE data format to "
         "the default CSV audio file format.")
 
     parser.add_argument("-i", "--input-path", default = "",
-        help = "The path to the TIMIT dataset.")
+        help = "The path to the COMMON-VOICE dataset.")
     parser.add_argument("-o", "--output-path", default = "",
         help = "The output path to save the dataset.")
     parser.add_argument("-v", "--verbose", default = False, action="store_true",
@@ -23,56 +23,69 @@ def main():
 
     setup_logger(arguments)
 
-    convert_timit_to_csv(arguments)
+    convert_common_voice_to_csv(arguments)
 
-def convert_timit_to_csv(arguments):
+def convert_common_voice_to_csv(arguments):
     all_files = get_all_files(arguments)
 
     if not os.path.exists(arguments["output_path"]):
         os.makedirs(arguments["output_path"])
+        os.makedirs(os.path.join(arguments["output_path"], "clips"))
 
     train_csv_path = os.path.join(arguments["output_path"], "train.csv")
-    test_csv_path = os.path.join(arguments["output_path"], "test.csv")
+    test_csv_path  = os.path.join(arguments["output_path"], "test.csv")
+    dev_csv_path   = os.path.join(arguments["output_path"], "dev.csv")
 
     with open(train_csv_path, "w") as output_train_file, \
-         open(test_csv_path,  "w") as output_test_file:
+         open(test_csv_path,  "w") as output_test_file, \
+         open(dev_csv_path,  "w") as output_dev_file:
         train_writer = csv.writer(output_train_file, delimiter=',', quotechar='"')
         test_writer = csv.writer(output_test_file, delimiter=',', quotechar='"')
+        dev_writer = csv.writer(output_test_file, delimiter=',', quotechar='"')
 
         for index, filename in enumerate(all_files):
-            label_filename = os.path.splitext(filename)[0] + '.TXT'
+            with open(filename) as input_file:
+                reader = csv.reader(input_file, delimiter='\t', quotechar='"')
 
-            new_filename = os.path.join(arguments["output_path"],
-                str(index) + ".wav")
+                next(reader)
 
-            shutil.copyfile(filename, new_filename)
-            logger.debug("Copying " + filename + " to " + new_filename)
+                for row in reader:
+                    label = row[2]
+                    path = row[1]
 
-            label = get_label(label_filename)
+                    base = os.path.split(filename)[0]
 
-            if is_test(label_filename):
-                test_writer.writerow([new_filename, label])
-            else:
-                train_writer.writerow([new_filename, label])
+                    utterance_path = os.path.join(base, "clips", path)
+
+                    logger.debug("Copying from " + utterance_path + " to " + new_filename)
+                    shutil.copy(utterance_path, new_filename)
+
+                    os.path.join(arguments["output_path"], "clips", path)
+
+                    if is_test(filename):
+                        test_writer.writerow([new_filename, label])
+                    elif is_dev(filename):
+                        dev_writer.writerow([new_filename, label])
+                    elif is_train(filename):
+                        train_writer.writerow([new_filename, label])
 
 def is_test(filename):
-    return filename.find("TEST") != -1
+    return filename.find("test") != -1
 
-def get_label(label_filename):
+def is_dev(filename):
+    return filename.find("dev") != -1
 
-    with open(label_filename) as label_file:
-        tokens = label_file.read().split(" ")
+def is_dev(filename):
+    return filename.find("train") != -1
 
-    return " ".join(tokens[2:]).strip()
-
-def is_wav(filename):
-    return os.path.splitext(filename)[1] == ".WAV"
+def is_tsv(filename):
+    return os.path.splitext(filename)[1] == ".tsv"
 
 def get_all_files(arguments):
     all_files = []
 
     for root, directories, files in os.walk(arguments["input_path"]):
-        all_files += [os.path.join(root, f) for f in files if is_wav(f)]
+        all_files += [os.path.join(root, f) for f in files if is_tsv(f)]
 
     return sorted(all_files)
 
