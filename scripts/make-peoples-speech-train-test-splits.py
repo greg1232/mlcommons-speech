79f0be29bbe61deca6d682dcb32b37806d940a48
greg1232 <greg@Landing-JKJG5J.local>
2020-10-24 01:47:56 -0700

    ENH: Simplify make dataset.

--- scripts/make-peoples-speech-train-test-splits.py
+++ scripts/make-peoples-speech-train-test-splits.py
@@ -97,6 +97,21 @@ def get_bucket_and_prefix(path):
 
     return parts[0], os.p.splitext(path)[1] == ".mp3" or os.path.splitext(path)[1] == ".wav"
 
+def split_all(path):
+    allparts = []
+    while True:
+        parts = os.path.split(path)
+        if parts[0] == path:  # sentinel for absolute paths
+            allparts.insert(0, parts[0])
+            break
+        elif parts[1] == path: # sentinel for relative paths
+            allparts.insert(0, parts[1])
+            break
+        else:
+            path = parts[0]
+            allparts.insert(0, parts[1])
+    return allparts
+
 def get_key(path):
     parts = split_all(path)
     return os.path.splitext(parts[-2] + "-" + parts[-1])[0]
