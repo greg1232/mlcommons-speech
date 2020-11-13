897b5dab7fd1723c06a004ba967929d977219c77
greg1232 <greg@Landing-JKJG5J.local>
2020-11-12 17:30:51 -0800

    ADD: google speech api

---
+++ scripts/add-google-speech-transcript.py
@@ -0,0 +1,117 @@
+
+from argparse import ArgumentParser
+import logging
+import csv
+import os
+import json
+import tarfile
+import io
+from smart_open import open
+from google.cloud import speech_v1p1beta1 as speech
+
+logger = logging.getLogger(__name__)
+
+def main():
+    parser = ArgumentParser(".")
+
+    parser.add_argument("-i", "--input-path", default = "gs://the-peoples-speech-west-europe/peoples-speech-v0.6/train.csv",
+        help = "The output path to load the dataset from.")
+    parser.add_argument("-o", "--output-path", default = "gs://the-peoples-speech-west-europe/peoples-speech-v0.6/train-with-transcripts.csv",
+        help = "The output path to save new transcripts.")
+    parser.add_argument("-v", "--verbose", default = False, action="store_true",
+        help = "Set the log level to debug, printing out detailed messages during execution.")
+
+    arguments = vars(parser.parse_args())
+
+    setup_logger(arguments)
+
+    add_transcripts(arguments)
+
+def add_transcript(arguments):
+    samples = load_csv(arguments["input_path"])
+
+    updated_samples = update_samples(samples)
+
+    with open(arguments["output_path"], "w", newline="") as output_csv_file:
+        csv_writer = csv.writer(output_csv_file, delimiter=',', quotechar='"')
+        for sample in updated_samples:
+            csv_writer.writerow(sample)
+
+
+def load_csv(csv_path):
+    new_samples = []
+    with open(csv_path) as csv_file:
+        reader = csv.reader(csv_file, delimiter=',', quotechar='"')
+
+        for row in reader:
+            path, caption = row[0], row[1]
+
+            metadata = {}
+            if len(row) >= 3:
+                metadata = json.loads(row[2])
+
+            yield {"path" : path, "caption" : caption, "metadata" : metadata}
+
+def update_samples(samples):
+    for sample in samples:
+        metadata = sample["metadata"]
+        metadata["google-speech-api-transcript"] = get_transcript(sample["path"])
+        logger.debug("Transcribed '" + sample["caption"] + "' to '" + metadata["google-speech-api-transcript"] + "'")
+        yield (sample["path"], sample["caption"], metadata)
+
+speech_client = speech.SpeechClient()
+
+def get_transcript(path):
+
+    # The language of the supplied audio
+    language_code = "en-US"
+
+    # Sample rate in Hertz of the audio data sent
+    sample_rate_hertz = 44100
+
+    # Encoding of audio data sent. This sample sets this explicitly.
+    # This field is optional for FLAC and WAV audio formats.
+    #encoding = speech.RecognitionConfig.AudioEncoding.MP3
+    config = {
+        "language_code": language_code,
+        "sample_rate_hertz": sample_rate_hertz,
+    #    "encoding": encoding,
+    }
+    audio = {"uri": path}
+
+    response = client.recognize(config=config, audio=audio)
+
+    transcript = ""
+
+    for result in response.results:
+        # First alternative is the most probable result
+        alternative = result.alternatives[0]
+        transcript += alternative.transcript
+
+    return transcript
+
+
+
+def setup_logger(arguments):
+
+   if arguments["verbose"]:
+       logger.setLevel(logging.DEBUG)
+   else:
+       logger.setLevel(logging.INFO)
+
+   ch = logging.StreamHandler()
+   ch.setLevel(logging.DEBUG)
+
+   # create formatter
+   formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+   # add formatter to ch
+   ch.setFormatter(formatter)
+
+   # add ch to logger
+   logger.addHandler(ch)
+
+if __name__ == "__main__":
+    main()
+
+
