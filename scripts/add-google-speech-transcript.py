b3fc1c4d932bc2bbdc3a8c3ce5f88e273a648c32
greg1232 <greg@Landing-JKJG5J.local>
2020-11-12 17:49:36 -0800

    ADD: google speech api

--- scripts/add-google-speech-transcript.py
+++ scripts/add-google-speech-transcript.py
@@ -73,7 +73,7 @@ def get_transcript(path):
     #encoding = speech.RecognitionConfig.AudioEncoding.MP3
     config = {
         "language_code": language_code,
-        "audio_channel_count": 2,
+        "enable_separate_recognition_per_channel": 2,
         #"sample_rate_hertz": sample_rate_hertz,
     #    "encoding": encoding,
     }
