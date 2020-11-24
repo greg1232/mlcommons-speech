f0755b607c8214a8e8897182c5eed6c551970e8a
greg1232 <greg@Landing-JKJG5J.local>
2020-11-24 03:29:18 -0800

    ENH: Added clean text script.

--- scripts/add-duration-metadata.py
+++ scripts/add-duration-metadata.py
@@ -119,6 +119,14 @@ def add_duration(arguments, path, metadata):
     os.remove(local_path)
     return metadata
 
+def sizeof_fmt(num, suffix='S'):
+    num = float(num)
+    for unit in ['','Ki','Mi','Gi','Ti','Pi','Ei','Zi']:
+        if abs(num) < 1024.0:
+            return "%3.1f%s%s" % (num, unit, suffix)
+        num /= 1024.0
+    return "%.1f%s%s" % (num, 'Yi', suffix)
+
 def setup_logger(arguments):
 
    if arguments["verbose"]:
